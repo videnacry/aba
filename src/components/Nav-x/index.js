ac1c672855523c240bca80b0aabf861cded44456
beron <bleink@outlook.es>
2021-07-28 20:10:00 +0200

    add nav-x logo and links for home and forum

--- src/components/Nav-x/index.js
+++ src/components/Nav-x/index.js
@@ -1,5 +1,7 @@
-import {ReactComponent as ListButton} from './list-button.svg'
+import { NavLink } from 'react-router-dom'
+import { ReactComponent as ListButton } from './list-button.svg'
 import { ReactComponent as SearchIcon } from './search.svg'
+import abaLogoSrc from './aba-logo.png'
 import Contents from './contents'
 const Nav = () => {
     return(
@@ -14,7 +16,7 @@ const Nav = () => {
             <div className="container-fluid col-12">
                 <a className="navbar-brand" href="#" samesite="strict">
                     <img 
-                        src="https://www.logolynx.com/images/logolynx/43/43d271e9b89dc1002fbbdc1b2114896b.png"
+                        src={abaLogoSrc}
                         alt=""
                         height="30px"
                         className="d-inline-block align-text-top"
@@ -26,7 +28,7 @@ const Nav = () => {
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            <NavLink className="nav-link" aria-current="page" to="/home" activeClassName="active">Inicio</NavLink>
                         </li>
                         <li className="nav-item">
                             <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#contents-modal">
@@ -36,7 +38,7 @@ const Nav = () => {
                             <Contents/>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link " href="#">Foro</a>
+                            <NavLink className="nav-link" to="/forum" activeClassName="active">Foro</NavLink>
                         </li>
                     </ul>
                 </div>
